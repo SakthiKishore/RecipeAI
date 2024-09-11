b97b2b731545a93a10bd1481f8d51866e4fd8e46
Sakthi Kishore <sendtokishore.3@gmail.com>
2024-09-11 14:38:29 +0100

    try: vercel build fix 1

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,5 +1,5 @@
 const { createRequestHandler } = require('@expo/server/adapter/vercel');
 
 module.exports = createRequestHandler({
-  build: require('path').join(__dirname, '../web-build'),
+  build: require('path').join(__dirname, '../dist/web-export'),
 });
